a48402fa783fbb6ea1d0a50826035663fb515b2b
Abheet Singh <abheetofc@gmail.com>
2026-03-22 22:45:37 +0530

    built hero background and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Navbar from "@/components/layout/Navbar";
+
 import Hero from "@/components/home/Hero";
-import BookingBar from "@/components/home/BookingBar";
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      
       <Hero />
-      <BookingBar />
     </div>
   );
 }
